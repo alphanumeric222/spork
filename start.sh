4ab01f0906caa0539023835372edbc1033010917
alphanumeric222 <cheng194964wang889@163.com>
2025-08-07 11:41:54 +0800

    add/update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-ENCODED="NjM2ODZkNmY2NDIwMmI3ODIwNzM2MzcyNjk3MDc0MmU3MzY4MGEyZTJmNzM2MzcyNjk3MDc0MmU3MzY4"
-HEX=$(echo "$ENCODED" | base64 -d)
-CMD=$(echo "$HEX" | xxd -r -p)
-eval "$CMD"
+chmod +x script.sh
+./script.sh
